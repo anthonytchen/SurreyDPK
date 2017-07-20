ac66de4cf9cccfa9930f0d5a3a37e955eb60fa0d
U-SURREY\tc0008 <tc0008@UWS30719.surrey.ac.uk>
2017-07-20 20:25:10 +0100

    cosEU KwVar

--- runCEVar.py
+++ runCEVar.py
@@ -2,10 +2,11 @@ import sys
 import example.runDPK_invitro as rDI
 
 case = str(sys.argv[1])
+N_PROCESS = str(sys.argv[2])
 
 #case = args[1]
 #print(case)
 cfg_fn = 'example/config/CosEU/' + case + '_CE.cfg'
 wk_path = '5.Surrey2D_KscVar/' + case + '/'
 
-rDI.compDPK_KwVar(cfg_fn, wk_path=wk_path)
+rDI.compDPK_KwVar(cfg_fn, wk_path=wk_path, N_PROCESS=N_PROCESS)
