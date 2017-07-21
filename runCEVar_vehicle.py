47d2d13d4278ced36acf6c2a3bfda1efddf5a2f7
Chen T Dr (Chem. Tc0008 Proc. Eng.) <tc0008@chemengsrv01.eps.surrey.ac.uk>
2017-07-21 10:32:48 +0100

    CosEU KwVar done.

--- runCEVar_vehicle.py
+++ runCEVar_vehicle.py
@@ -3,10 +3,9 @@ import example.runDPK_invitro as rDI
 
 case = str(sys.argv[1])
 N_PROCESS = int(sys.argv[2])
+vehicle = str(sys.argv[3])
 
-#case = args[1]
-#print(case)
-cfg_fn = 'example/config/CosEU/' + case + '_CE.cfg'
-wk_path = '5.Surrey2D_KscVar/' + case + '/'
+cfg_fn = 'example/config/CosEU/' + case + '_CE_VH=' + vehicle +'.cfg'
+wk_path = '5.Surrey2D_KscVar/' + case + 'VH=' + vehicle + '/'
 
 rDI.compDPK_KwVar(cfg_fn, wk_path=wk_path, N_PROCESS=N_PROCESS)
